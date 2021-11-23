e98586d5405f4269bc1fc94fa8f738efe7389b3f
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-23 16:59:37 -0500

    Implement and Test And Test File Download

--- src/main/java/com/bahrath/rest/restfileprocessing/controller/FileController.java
+++ src/main/java/com/bahrath/rest/restfileprocessing/controller/FileController.java
@@ -1,13 +1,16 @@
 package com.bahrath.rest.restfileprocessing.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 @RestController
 public class FileController {
@@ -19,6 +22,14 @@ public class FileController {
     public boolean upload(@RequestParam("file") MultipartFile file) throws IOException {
         file.transferTo(new File(UPLOAD_DIR + file.getOriginalFilename()));
         return true;
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<byte[]> download(@PathVariable("fileName") String fileName) throws IOException {
+        byte[] fileData = Files.readAllBytes(new File(UPLOAD_DIR + fileName).toPath());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        return new ResponseEntity<byte[]>(fileData, headers, HttpStatus.OK);
     }
 
 }
