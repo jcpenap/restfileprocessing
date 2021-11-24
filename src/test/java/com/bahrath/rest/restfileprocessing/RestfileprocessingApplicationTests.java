48ed4617235c26a1e826e3ec11da66958f638bc9
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-24 09:45:28 -0500

    Upload Client Test

--- src/test/java/com/bahrath/rest/restfileprocessing/RestfileprocessingApplicationTests.java
+++ src/test/java/com/bahrath/rest/restfileprocessing/RestfileprocessingApplicationTests.java
@@ -1,13 +1,39 @@
 package com.bahrath.rest.restfileprocessing;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class RestfileprocessingApplicationTests {
 
+	public static final String FILE_UPLOAD_URL = "http://localhost:8080/upload";
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	void testUpload() {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		body.add("file", new ClassPathResource("foto_juan.jpg"));
+
+		HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(body, headers);
+
+		ResponseEntity<Boolean> response = restTemplate.postForEntity(FILE_UPLOAD_URL, httpEntity, Boolean.class);
+
+		System.out.println(response.getBody());
+
 	}
 
 }
