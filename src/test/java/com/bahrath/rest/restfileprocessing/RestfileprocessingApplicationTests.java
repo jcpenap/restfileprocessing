1e109c276bd113bd86dd058cc073d048ccc7e10f
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-24 10:20:23 -0500

    Download Client Test

--- src/test/java/com/bahrath/rest/restfileprocessing/RestfileprocessingApplicationTests.java
+++ src/test/java/com/bahrath/rest/restfileprocessing/RestfileprocessingApplicationTests.java
@@ -2,22 +2,32 @@ package com.bahrath.rest.restfileprocessing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
 
 @SpringBootTest
 class RestfileprocessingApplicationTests {
 
 	public static final String FILE_UPLOAD_URL = "http://localhost:8080/upload";
+	public static final String FILE_DOWNLOAD_URL = "http://localhost:8080/download/";
 	@Autowired
 	private RestTemplate restTemplate;
+
+	@Value("${uploadDir}")
+	private String UPLOAD_DIR;
+
+	@Value("${downloadDir}")
+	private String DOWNLOAD_DIR;
 
 	@Test
 	void testUpload() {
@@ -34,6 +44,20 @@ class RestfileprocessingApplicationTests {
 
 		System.out.println(response.getBody());
 
+	}
+
+	@Test
+	void testDownload() throws IOException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_OCTET_STREAM));
+
+		HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
+
+		String fileName = "foto_juan.jpg";
+
+		ResponseEntity<byte[]> response = restTemplate.exchange(FILE_DOWNLOAD_URL+fileName, HttpMethod.GET, httpEntity, byte[].class);
+
+		Files.write(Paths.get(DOWNLOAD_DIR +fileName), response.getBody());
 	}
 
 }
